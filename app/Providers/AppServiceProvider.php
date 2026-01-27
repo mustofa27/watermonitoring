f2d33da1d33cc5d6044605894f1b74ea6d9d0567
Ahmad Mustofa <mustofaahmad@poltera.ac.id>
2026-01-27 14:47:26 +0700

    fixing bug in view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Use Bootstrap pagination theme
+        Paginator::useBootstrapFive();
+        
         Blade::directive('num', function ($expression) {
             return "<?= num_id($expression) ?>";
         });
